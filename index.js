36e44efc3b7688bcd6e1803f3dc71c6e9bf3e783
Jon Johnson <jonjonsonjr@gmail.com>
2014-06-26 16:19:39 -0400

    Made a small change to how it loads commits
    Instead of loading everything synchronously when the program begins,
    this just loads the list of hashes and adds data to the consumer as it
    reads each commit. This way, we won't crash on some really huge repos,
    and there is no wait before typing can happen.

--- index.js
+++ index.js
@@ -3,38 +3,59 @@ var parse = require('diff-parse');
 var detect = require('language-detect');
 var tempWrite = require('temp-write');
 var Repo = require('git').Repo;
+var Commit = require('git').Commit;
 var Consumer = require('./lib/consumer');
 var Editor = require('./lib/editors/ace');
-var queue = [];
 
 var args = process.argv.slice(2);
 // give it the path to a repo on your local machine
 // TODO: Add cloning so we can point it at github
 var repoPath = args[0] || '.';
+var editor = new Editor();
+var consumer = new Consumer([], editor, {speed: 1});
 
-// Load up a repo and walk through the logs
 new Repo(repoPath, function (err, repo) {
-  repo.log(function (err, logs) {
-    logs.reverse();
+  if (err) return console.log(err);
+  loadHashes(repo, function (err, hashes) {
+    if (err) return console.log(err);
 
-    async.eachSeries(logs, function (log, callback) {
-      var changes = log.filechanges;
-      var keys = Object.keys(changes);
+    function foo() {
+      var hash = hashes.pop();
+      var args = ['--raw', '--no-abbrev', '--numstat', '--pretty=raw', hash];
+      repo.git.call_git('', 'show', '', {color: 'never'}, args, function (err, text) {
+        if (err) return console.log(err);
+        var commit = Commit.list_from_string(repo, text)[0];
+        var changes = commit.filechanges;
+        var keys = Object.keys(changes);
 
-      if (keys.length > 50) return callback();
+        // just skip over the commit if there are more than 50 files
+        if (keys.length > 50) return foo();
 
-      async.eachSeries(keys, function (key, cb) {
-        handleFile(repo, changes[key], cb);
-      }, function (err) {
-        done(log);
-        callback();
+        async.eachSeries(keys, function (key, cb) {
+          handleFile(repo, changes[key], cb);
+        }, function (err) {
+          done(commit);
+          if (hashes.length > 0) {
+            process.nextTick(foo);
+          }
+        });
       });
-    }, function (err) {
-      var editor = new Editor();
-      var consumer = new Consumer(queue, editor, {speed: 1});
-    });
+    }
+
+    foo();
   });
 });
+
+// array of hashes in chronological order
+function loadHashes(repo, cb) {
+  // 25575 is max number of commits before "Error: stdout maxBuffer exceeded."
+  var args = ['--max-count=25575', 'HEAD'];
+  var options = {color: 'never'};
+  repo.git.call_git('', 'rev-list', '', options, args, function (err, res) {
+    if (err) return cb(err);
+    cb(null, res.split('\n'));
+  });
+}
 
 // Push events based on what happened to each file in this commit
 function handleFile(repo, change, cb) {
@@ -45,13 +66,13 @@ function handleFile(repo, change, cb) {
       getFileType(blob.data, change.path, function (err, type) {
         if (err) return console.log(err);
 
-        queue.push({
+        consumer.add({
           action: "open file",
           file: change.path,
           type: type,
           data: ""
         });
-        queue.push({
+        consumer.add({
           action: "type",
           data: blob.data
         });
@@ -59,7 +80,7 @@ function handleFile(repo, change, cb) {
       });
     });
   } else if (change.what == 'D') {
-    queue.push({
+    consumer.add({
       action: "delete file",
       file: change.path
     });
@@ -73,18 +94,18 @@ function handleFile(repo, change, cb) {
         getFileType(blob.data, change.path, function (err, type) {
           if (err) return console.log(err);
 
-          queue.push({
+          consumer.add({
             action: "open file",
             file: change.path,
             type: type,
             data: blob.data
           });
-          queue.push({
+          consumer.add({
             action: "edit",
             blob: change.a_blob,
             diff: parse(out)
           });
-        cb();
+          cb();
         });
       });
     });
@@ -98,7 +119,7 @@ function done(log) {
   var isMerge = log.parents.length > 1;
 
   if (isMerge) {
-    queue.push({
+    consumer.add({
       action: "git merge",
       message: log.message
     });
@@ -108,12 +129,12 @@ function done(log) {
     });
 
     if (adds.length > 0) {
-      queue.push({
+      consumer.add({
         action: "git add",
         files: adds
       });
     }
-    queue.push({
+    consumer.add({
       action: "git commit",
       message: log.message,
       changes: changes
@@ -123,13 +144,19 @@ function done(log) {
 
 function getFileType(content, name, cb) {
   tempWrite(content, name, function (err, path) {
+    if (err) return cb(err);
     detect(path, function (err, type) {
-      if (err) return eb(err);
-      if (type === 'C' || type === 'C++') {
-        // Make ace happy
+      if (err) return cb(err);
+
+      type = (type) ? type : 'text';
+      type = type.toLowerCase().replace(/[ -]/g, '_');
+      type = (type === 'objective_c') ? 'objectivec' : type;
+      type = (type === 'shell') ? 'nix' : type;
+
+      if (type === 'c' || type === 'c++') {
         type = 'c_cpp';
       }
-      cb(null, type.toLowerCase().replace(/[ -]/g, '_'));
+      cb(null, type);
     });
   });
 }
